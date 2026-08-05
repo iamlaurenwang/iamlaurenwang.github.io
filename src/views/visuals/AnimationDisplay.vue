feb7d7aa4d2049549a12d039dbfc15baf0a524c8
iamlaurenwang <iamlaurenwang@gmail.com>
2026-08-05 11:39:46 +0800

    chore

---
+++ src/views/visuals/AnimationDisplay.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="p-8 text-neutral-700">
+    <h1 class="font-serif text-2xl">Animation</h1>
+    <p class="mt-2 text-neutral-500">Coming soon.</p>
+  </section>
+</template>
